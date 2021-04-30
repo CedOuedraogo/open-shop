6f22dbd3333f3a7b47edd64a20776e76ed96dea9
OUEDRAOGO Arnold Cedric <cedarnoldouedraogo@gmail.com>
2021-04-30 16:32:56 +0000

    config heroku

--- database/seeders/ProduitSeeder.php
+++ database/seeders/ProduitSeeder.php
@@ -12,23 +12,18 @@ class ProduitSeeder extends Seeder
      *
      * @return void
      */
-   /* public function run()   *pour renseigner nos données
-    {
-        $produit= Produit::create([
-            'designation'=>'chemise',
-            'prix'=>5000,
-            'description'=>'ceci est la description de chemise',
-            'quantite'=>50,
-
-        ]);
-    }*/
-
-    
-
     public function run()
     {
-        Produit::Factory(50)->create(); //du fait kon peut pas executer le factory direct dans le T on lamene dans le Seed our execute
-
-        
+        $produit = Produit::create([
+             'designation' => 'chemise',
+             'prix' => 5000,
+             'description' => 'ceci est la description de chemise',
+             'quantite' => 50,
+         ]);
     }
+
+    /*public function run()
+    {
+        Produit::Factory(50)->create(); //du fait kon peut pas executer le factory direct dans le T on lamene dans le Seed our execute
+    }*/
 }
